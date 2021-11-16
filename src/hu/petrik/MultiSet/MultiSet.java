3a7b50371994a4bd8510ad800e5fa39d58e68450
szotaksimon <szotak.simon16@gmail.com>
2021-11-16 20:25:27 +0100

    MultiSet kis rész

--- src/hu/petrik/MultiSet/MultiSet.java
+++ src/hu/petrik/MultiSet/MultiSet.java
@@ -1,6 +1,27 @@
 package hu.petrik.MultiSet;
 
-public class MultiSet {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiSet<T> {
+    private List<T> elements;
+
+    public MultiSet(){
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element){
+        this.elements.add(element);
+    }
+    private int db(T element){
+        int elementDb = 0;
+        for(T elem : this.elements){
+            if (elem == element){
+                elementDb++;
+            }
+        }
+        return elementDb;
+    }
 
 
 
